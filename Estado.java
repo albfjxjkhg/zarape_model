7b1039729882e5f9377d3f0f31ae1c7f6f1b5965
albfjxjkhg <76073805+albfjxjkhg@users.noreply.github.com>
2024-10-20 15:31:42 -0600

    Add files via upload

---
+++ Estado.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.utl.dsm403.zarape.model;
+
+public class Estado {
+    private int idEstado;
+    private String nombre;
+
+    public Estado(int idEstado, String nombre) {
+        this.idEstado = idEstado;
+        this.nombre = nombre;
+    }
+
+    public int getIdEstado() {
+        return idEstado;
+    }
+
+    public void setIdEstado(int idEstado) {
+        this.idEstado = idEstado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
+
